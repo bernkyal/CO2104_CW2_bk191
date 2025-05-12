234eb9ae82460a01c9f87322cd9e4aa66ff1cc04
bernkyal <bernardkya@gmail.com>
2025-05-12 18:01:08 +0100

    final touches

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Review from './pages/Review';
 import UserProfile from './pages/UserProfile';
